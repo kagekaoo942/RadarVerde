6a0770379250b3fa44d086109dfddbfc475d2848
kagekaoo942 <martinacevedo@liceovvh.cl>
2026-08-24 10:20:11 -0400

    terminar

--- RadarVerde-main/README.md
+++
@@ -1,2 +0,0 @@
-# RadarVerde
-emprendimiento
---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, abort, render_template, send_from_directory
+
+app = Flask(__name__, static_folder=None)
+
+
+@app.route('/static/<path:filename>', endpoint='static')
+def static_files(filename):
+    folder, separator, relative_path = filename.partition('/')
+    folders = {'css': 'css', 'images': 'images', 'js': 'JS'}
+    if not separator or folder not in folders:
+        abort(404)
+    return send_from_directory(folders[folder], relative_path)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/mapa')
+def mapa():
+    return render_template('mapa.html')
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
